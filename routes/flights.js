349fbe3bac2d29c514a4598c2a6a7f5898df16fc
Darren Lewis <darren@octono.com>
2017-01-01 14:24:12 +0000

    Basic working API with disk cache for development

---
+++ routes/flights.js
@@ -0,0 +1,69 @@
+var express = require('express');
+var router = express.Router();
+var request = require('request');
+var jsonfile = require('jsonfile');
+var fs = require("fs");
+var formurlencoded = require('form-urlencoded');
+
+var cacheEnabled = false;
+
+var options = {
+  uri: "https://www.birminghamairport.co.uk/Api/FidApi/GetFlights",
+  body: formurlencoded({
+    "Arrivals": {
+      flightType: "Arrivals",
+      searchType: "Destination",
+      query: "",
+      timespan: "EightHours"
+    },
+    "Departures": {
+      flightType: "Departures",
+      searchType: "Destination",
+      query: "",
+      timespan: "EightHours"
+    },
+  }),
+  method: "POST",
+  headers: {
+    "Content-Type": "application/x-www-form-urlencoded","accept" : "application/json"  
+  }
+};
+
+let filePath = process.cwd() + '\\cache\\data.json';
+
+router.get('/', function(req, res, next) {
+
+  fs.readFile(filePath,'utf8', (err,data) => {
+    if(!err && data.length > 0 && cacheEnabled) {
+      console.log("serving from disk");
+      data = JSON.parse(data);
+      data.source = "disk";
+      res.json(data);
+    } else {
+      getDataFromServer(res);
+    }
+  });
+});
+
+router.get('/arrivals',(req, res, next) => {
+  res.render('index',{title: 'ARRIVALS'});
+});
+
+router.get('/departures',(req, res, next) => {
+  res.render('index',{title: 'DEPARTURES'});
+});
+
+function getDataFromServer(res){
+  console.log("requesting from server");
+  request(options, function(error, response, body){
+    if(!error){
+      let data = JSON.parse(body);
+      data.source = "server";
+      if(data.success){
+        jsonfile.writeFile(filePath,data);
+        res.json(data);
+      }
+    }
+  });
+}
+module.exports = router;
