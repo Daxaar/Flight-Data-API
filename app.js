89d7d40b9944108b6813a06066649873c60aeb96
Darren Lewis <darren@octono.com>
2017-01-01 19:14:19 +0000

    Fixed eslint violations

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
-var flights = require("./routes/flights");
+var flights = require('./routes/flights');
 
 var app = express();
 
@@ -25,7 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
-app.use("/flights", flights);
+app.use('/flights', flights);
 
 
 // catch 404 and forward to error handler
@@ -36,7 +36,7 @@ app.use(function(req, res, next) {
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- routes/flights.js
+++ routes/flights.js
@@ -2,44 +2,44 @@ var express = require('express');
 var router = express.Router();
 var request = require('request');
 var jsonfile = require('jsonfile');
-var fs = require("fs");
+var fs = require('fs');
 var formurlencoded = require('form-urlencoded');
 
 var cacheEnabled = true;
 
 var options = {
-  uri: "https://www.birminghamairport.co.uk/Api/FidApi/GetFlights",
+  uri: 'https://www.birminghamairport.co.uk/Api/FidApi/GetFlights',
   body: formurlencoded({
-    "Arrivals": {
-      flightType: "Arrivals",
-      searchType: "Destination",
-      query: "",
-      timespan: "EightHours"
+    'Arrivals': {
+      flightType: 'Arrivals',
+      searchType: 'Destination',
+      query: '',
+      timespan: 'EightHours'
     },
-    "Departures": {
-      flightType: "Departures",
-      searchType: "Destination",
-      query: "",
-      timespan: "EightHours"
+    'Departures': {
+      flightType: 'Departures',
+      searchType: 'Destination',
+      query: '',
+      timespan: 'EightHours'
     },
   }),
-  method: "POST",
+  method: 'POST',
   headers: {
-    "Content-Type": "application/x-www-form-urlencoded","accept" : "application/json"  
+    'Content-Type': 'application/x-www-form-urlencoded','accept' : 'application/json'
   }
 };
 
 let filePath = process.cwd() + '\\cache\\data.json';
 
-router.get('/', function(req, res, next) {
+router.get('/', (req, res) => {
   getData(data => res.json(data));
 });
 
-router.get('/arrivals',(req, res, next) => {
+router.get('/arrivals',(req, res) => {
   res.render('index',{title: 'ARRIVALS'});
 });
 
-router.get('/departures',(req, res, next) => {
+router.get('/departures',(req, res) => {
   res.render('index',{title: 'DEPARTURES'});
 });
 
@@ -59,7 +59,7 @@ function readFromCache(cb){
   fs.readFile(filePath,'utf8', (err,data) => {
     if(!err && data.length > 0 && cacheEnabled) {
       data = JSON.parse(data);
-      data.source = "disk";
+      data.source = 'disk';
       cb(data);
     }
     cb(null);
@@ -70,7 +70,7 @@ function getDataFromServer(cb){
   request(options, function(error, response, body){
     if(!error){
       let data = JSON.parse(body);
-      data.source = "server";
+      data.source = 'server';
       if(data.success){
         jsonfile.writeFile(filePath,data);
         cb(data);
