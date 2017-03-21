c5c32289492ccee10bc99223cbbcc046b8666c13
Darren Lewis <darren@octono.com>
2017-03-21 20:28:06 +0000

    Add CORS header

--- app.js
+++ app.js
@@ -53,4 +53,10 @@ app.use((err, req, res) => {
   res.render('error');
 });
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 module.exports = app;
