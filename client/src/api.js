4cf0dbe1f17edab23e2e0523b010b49b83d232c2
Darren Lewis <darren@octono.com>
2018-12-28 10:30:15 +0000

    Remove console logs

--- client/src/api.js
+++ client/src/api.js
@@ -4,12 +4,10 @@ const baseUrl = "http://localhost:3001/flights/";
 
 export default {
     async getArrivals() {
-        console.log("arrivals");
         const response = await Axios.get(baseUrl + "arrivals?cache=1");
         return response.data.arrivals;
     },
     getDepartures() {
-        console.log("departures");
         Axios.get(baseUrl + "departures?cache=1")
             .then(data => data);
     }
