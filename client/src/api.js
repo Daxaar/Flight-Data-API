7f363ce2e17f3eea7e0229566192ca3f6ca28d48
Darren Lewis <darren@octono.com>
2018-12-28 21:43:13 +0000

    Add sort to arrivals and departures at api

--- client/src/api.js
+++ client/src/api.js
@@ -5,10 +5,10 @@ const baseUrl = "http://localhost:3001/flights/";
 export default {
     async getArrivals() {
         const response = await Axios.get(baseUrl + "arrivals?cache=1");
-        return response.data.arrivals;
+        return response.data.arrivals.sort((a, b) => a.EstimatedTime - b.EstimatedTime);
     },
     async getDepartures() {
         const response = await Axios.get(baseUrl + "departures?cache=1")
-        return response.data.departures;
+        return response.data.departures.sort((a, b) => a.EstimatedTime - b.EstimatedTime);
     }
 }
