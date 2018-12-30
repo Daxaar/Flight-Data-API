160f8ca0cdd57db64972c3c4ca23a962d1b463d7
Darren Lewis <darren@octono.com>
2018-12-30 16:51:22 +0000

    Read API url from environment file

--- client/src/api.js
+++ client/src/api.js
@@ -1,11 +1,9 @@
 import Axios from "axios";
 
-//const baseUrl = "http://localhost:3001/flights/";
 const baseUrl = `${process.env.VUE_APP_API_URL}/flights/`;
 
 export default {
     async getArrivals() {
-        //console.log("baseUrl", baseUrl);
         const response = await Axios.get(baseUrl + "arrivals?cache=1");
         return response.data.arrivals.sort((a, b) => a.EstimatedTime - b.EstimatedTime);
     },
